bf98672d6bb03053e69a1c85d0a25eaa616f2c52
mengzzz0628 <yan_ni2015@sina.com>
2017-11-26 19:01:03 -0800

    bio is not updated. get redirected to goclass page

--- lib/SylSpace/Controller/AuthBiosave.pm
+++ lib/SylSpace/Controller/AuthBiosave.pm
@@ -14,7 +14,13 @@ use SylSpace::Model::Controller qw(standard global_redirect);
 ################################################################
 
 post '/auth/biosave' => sub {
+
   my $c = shift;
+print '\n\n';
+	print $c;
+print '\n\n';
+print standard($c);
+print '\n\n';
   (my $course = standard( $c )) or return global_redirect($c);
 
   biosave( $c->session->{uemail}, $c->req->body_params->to_hash ) or die "evil bio submission\n";
