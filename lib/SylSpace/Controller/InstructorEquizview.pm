0b927b66fe305444d5df98abd065af505542a908
ivo welch <ivo.welch@gmail.com>
2026-01-20 22:46:36 -0800

    Fix InstructorEquizview - add feature pragmas, fix route

--- lib/SylSpace/Controller/InstructorEquizview.pm
+++ lib/SylSpace/Controller/InstructorEquizview.pm
@@ -8,12 +8,16 @@ use Mojolicious::Lite;
 use lib qw(.. ../..); ## make syntax checking easier
 use strict;
 
+use feature ':5.20';
+use feature 'signatures';
+no warnings qw(experimental::signatures);
+
 use SylSpace::Model::Model qw(sudo equizpreview);
 use SylSpace::Model::Controller qw(global_redirect standard);
 
 ################################################################
 
-get 'instructor/equizview' => sub {
+get '/instructor/equizview' => sub {
   my $c = shift;
   (my $course = standard( $c )) or return global_redirect($c);
 
